0c38e423d1ff474a66d62689b3d8b0670cafa86c
Jason Sturges <jason@jasonsturges.com>
2024-10-16 23:11:19 -0500

    Vite config
    Simplified root

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import * as path from "path";
 
 export default defineConfig({
   build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-      },
-    },
+    root: "src",
   },
 });
